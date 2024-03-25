4949e35e217ea90e599e2ceceb1d9bc114c2ab21
Dan Moffat <danmoffat@gmail.com>
2024-03-25 21:59:06 +0000

    Added basic redux store boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './store';
 import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
